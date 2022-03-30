e5ecb50183d905d76c2a47fb886f2ddc5f725a91
Andrey Mospanov <mospanov-andrey@ya.ru>
2022-03-30 17:38:06 +0500

    Добавьте файлы проекта.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+/*Задание 1. Написать рекурсивную функцию нахождения
+степени числа.*/
+int Power(int num, int power)
+{
+    if (power == 0)
+    {
+        return 1;
+    }
+    return num * Power(num, power - 1);
+}
+
+int main()
+{
+    int num, power;
+    cout << "Enter num ";
+    cin >> num;
+    cout << "\nEnter power ";
+    cin >> power;
+    cout <<"Result = " <<Power(num, power) << endl;
+}
+
---
+++ Task2/Task2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+/*Задание 2. Написать рекурсивную функцию, которая
+выводит N звезд в ряд, число N задает пользователь. Проиллюстрируйте работу функции примером.*/
+void PrintStars(int num)
+{
+	cout << "*";
+	if(num > 1)
+	PrintStars(num - 1);
+}
+int main()
+{
+	int num;
+	cout << "Enter number ";
+	cin >> num;
+	PrintStars(num);
+}
